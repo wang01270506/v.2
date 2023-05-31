4d30c83169447cb621acbf53366c3fea3614a335
s1100381 <s1100381@gm.pu.edu.tw>
2023-05-31 09:43:58 +0800

    第一次

---
+++ app/src/main/java/tw/edu/pu/csim/helen/MainActivity.kt
@@ -0,0 +1,41 @@
+package tw.edu.pu.csim.helen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.helen.ui.theme.資管2A王思婷Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            資管2A王思婷Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    資管2A王思婷Theme {
+        Greeting("Android")
+    }
+}
