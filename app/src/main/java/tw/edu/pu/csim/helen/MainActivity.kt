a7c2666b50b16d6033f20fffc7ef747b31cd0115
s1100381 <s1100381@gm.pu.edu.tw>
2023-05-31 10:13:32 +0800

    第一次

--- app/src/main/java/tw/edu/pu/csim/helen/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/helen/MainActivity.kt
@@ -1,15 +1,23 @@
 package tw.edu.pu.csim.helen
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tw.edu.pu.csim.helen.ui.theme.資管2A王思婷Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,13 +37,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    Row(){
+        Text(text = "作者:資管二A 王思婷")
+        Image(
+            painterResource(id = R.drawable.map),
+            contentDescription ="我的照片",
+            modifier = Modifier.size(450.dp))
+
+    }
+
+
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     資管2A王思婷Theme {
-        Greeting("Android")
+
     }
 }
